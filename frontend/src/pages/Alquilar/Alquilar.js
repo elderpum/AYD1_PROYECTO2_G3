5cb34d1580f0c48a75ea06369f8ceaab9d16b90b
Mario <3017701820101@ingenieria.usac.edu.gt>
2023-10-10 16:28:51 -0600

    vista gestionar cuotas

--- frontend/src/pages/Alquilar/Alquilar.js
+++ frontend/src/pages/Alquilar/Alquilar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
+import '../../components/Titulo.css';
 
-import { Sidebar } from '../../components/Sidebar'
 import { Grid, Button } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -9,7 +9,6 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
 import { BsArrowLeft } from "react-icons/bs";
-
 
 export function Alquilar() {
     const [fechaInicio, setFechaInicio] = useState('');
@@ -56,65 +55,62 @@ export function Alquilar() {
     });
     
     return (
-        <Container>
-            <Sidebar isOrganizador={false} tipoUsuario={""}/>
-            <BodyContent>
-                <ContainerButton>
-                    <ThemeProvider theme={theme}>
-                        <Button variant="contained" color="salmon" onClick={regresar} style={{marginRight: 20}}>
-                            <BsArrowLeft style={{color: "#3DF28B", fontSize: "1.5em" }}/>
-                        </Button>
-                    </ThemeProvider>
-                    <h2 className='heads'> {"Alquilar Vehiculo"} </h2>
-                </ContainerButton>
-                <Info>
-                    <TituloInfo> Mercedez Benz - MODELO </TituloInfo>
-                    <Grid sx={{ flexGrow: 1 }} container spacing={2}>
-                        <Grid item xs={12}>
-                            <Grid container justifyContent="center" spacing={0.5}>
-                                <Grid item xs={4}>
-                                    foto del vehiculo
-                                </Grid>
-                                <Grid item xs={4}>
-                                    <h6 className='align_l'> Transmisión: {'evento.fecha'}</h6>  
-                                    <h6 className='align_l'> Asientos: {'evento.duracion'} </h6>
-                                    <h6 className='align_l'> Combustible: {'evento.ubicacion'} </h6>
-                                    <h6 className='align_l'> Estado: {'evento.formato'} </h6>
-                                    <h6 className='align_l'> Categoría: {'evento.costo'} </h6>
-                                    <h6 className='align_l'> Cuota por día: Q 20.00</h6>
-                                </Grid>
-                                <Grid item xs={4}>
-                                    <p className='align_l'>  Cuota de Alquiler: <h4 className='align_l'>Q 100.00 </h4> </p>
-                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                        <DatePicker 
-                                            label="Inicio"
-                                            onChange={(newValue) => configFechaInicio(newValue)}
-                                            views={["year", "month", "day"]}
-                                            format="YYYY-MM-DD"
-                                            slotProps={{ textField: { size: 'small' } }}
-                                        />
-                                    </LocalizationProvider>
-                                    <p></p>
-                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                        <DatePicker 
-                                            label="Final"
-                                            onChange={(newValue) => configFechaFinal(newValue)}
-                                            views={["year", "month", "day"]}
-                                            format="YYYY-MM-DD"
-                                            slotProps={{ textField: { size: 'small' } }}
-                                        />
-                                    </LocalizationProvider>
-                                    <p></p>
-                                    <Button variant="outlined" size="small" color="success">
-                                        Alquilar
-                                    </Button>
-                                </Grid>
+        <BodyContent>
+            <ContainerButton>
+                <ThemeProvider theme={theme}>
+                    <Button variant="contained" color="salmon" onClick={regresar} style={{marginRight: 20}}>
+                        <BsArrowLeft style={{color: "#3DF28B", fontSize: "1.5em" }}/>
+                    </Button>
+                </ThemeProvider>
+                <h2 className='heads'> {"Alquilar Vehiculo"} </h2>
+            </ContainerButton>
+            <Info>
+                <TituloInfo> Mercedez Benz - MODELO </TituloInfo>
+                <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+                    <Grid item xs={12}>
+                        <Grid container justifyContent="center" spacing={0.5}>
+                            <Grid item xs={4}>
+                                foto del vehiculo
+                            </Grid>
+                            <Grid item xs={4}>
+                                <h6 className='align_l'> Transmisión: {'evento.fecha'}</h6>  
+                                <h6 className='align_l'> Asientos: {'evento.duracion'} </h6>
+                                <h6 className='align_l'> Combustible: {'evento.ubicacion'} </h6>
+                                <h6 className='align_l'> Estado: {'evento.formato'} </h6>
+                                <h6 className='align_l'> Categoría: {'evento.costo'} </h6>
+                                <h6 className='align_l'> Cuota por día: Q 20.00</h6>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <p className='align_l'>  Cuota de Alquiler: <h4 className='align_l'>Q 100.00 </h4> </p>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DatePicker 
+                                        label="Inicio"
+                                        onChange={(newValue) => configFechaInicio(newValue)}
+                                        views={["year", "month", "day"]}
+                                        format="YYYY-MM-DD"
+                                        slotProps={{ textField: { size: 'small' } }}
+                                    />
+                                </LocalizationProvider>
+                                <p></p>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DatePicker 
+                                        label="Final"
+                                        onChange={(newValue) => configFechaFinal(newValue)}
+                                        views={["year", "month", "day"]}
+                                        format="YYYY-MM-DD"
+                                        slotProps={{ textField: { size: 'small' } }}
+                                    />
+                                </LocalizationProvider>
+                                <p></p>
+                                <Button variant="outlined" size="small" color="success">
+                                    Alquilar
+                                </Button>
                             </Grid>
                         </Grid>
                     </Grid>
-                </Info>
-            </BodyContent>
-        </Container>
+                </Grid>
+            </Info>
+        </BodyContent>
     )
 }
 
@@ -141,6 +137,22 @@ font-size: 22px;
 justify-content: space-between;
 font-family: 'Jost', sans-serif;
 margin-bottom: 25px;
+`
+
+const Container2 = styled.div`
+position: sticky;
+top: 0;
+flex: 0.2;
+height: 100%;
+min-height: 100vh;
+background-color: #181818;
+color: #b3b3b3;
+min-width: 240px;
+-webkit-box-shadow: 4px 5px 20px -7px rgba(0, 0, 0, 0.65);
+-moz-box-shadow: 4px 5px 20px -7px rgba(0, 0, 0, 0.65);
+box-shadow: 4px 5px 20px -7px rgba(0, 0, 0, 0.65);
+text-size-adjust: none;
+text-size-adjust: none;
 `
 
 const Info = styled.div`
