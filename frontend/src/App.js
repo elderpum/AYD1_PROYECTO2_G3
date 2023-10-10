69cf75ef95506bf406bfb43ae8f2ece4ff96c670
Ferchojrjose <ferchoserrano0@gmail.com>
2023-10-10 13:47:10 -0600

    Configuracion general del SideBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Inventario from './pages/Inventario/Inventario';
 import { RegistroCliente } from './pages/registroCliente/RegistroCliente';
 import { RegistroEmpleado } from './pages/registroEmpleado/RegistroEmpleado';
-import { Alquilar } from './pages/Alquilar/Alquilar';
-import { GestionarCosto } from './pages/GestionarCosto/GestionarCosto';
+import { AlquimovilRouter } from './router/AlquimovilRouter';
 
 
 function App() {
@@ -12,12 +10,12 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/registroCliente" element={<RegistroCliente />} />
-          <Route path="/registroEmpleado" element={<RegistroEmpleado />} />
 
-          <Route path="/inicio" element={<Inventario />} /> {/** VISUALIZACION DE VEHICULOS */}
-          <Route path="/alquilar/:id" element={<Alquilar />} /> {/** ALQUILAR UN VEHICULO */}
-          <Route path="/gestionarCosto/:id" element={<GestionarCosto />} /> {/** GestionarCosto UN VEHICULO */}
+          <Route path="/" element={<RegistroCliente />} />
+          {/* <Route path="/registroCliente" element={<RegistroCliente />} /> */}
+          <Route path="/registroEmpleado" element={<RegistroEmpleado />} />
+          <Route path="/*" element={<AlquimovilRouter />} /> {/* RUTAS HACIA NAVBAR */}
+
         </Routes>
       </BrowserRouter>
     </div>
