de1a8de9d06b34ed6533d74c173e3d84c04e92cb
EsdrasToc <69408116+EsdrasToc@users.noreply.github.com>
2023-10-30 00:30:04 -0600

    [ADD] respuesta y visualizacion de solicitudes
    Se pueden responder las solicitudes, asi com over el historial completo de las solicitudes dependiendo de los privilegios por tipo de usuario

---
+++ backend/Controllers/controllerRequest.js
@@ -0,0 +1,109 @@
+const service = require('../Services/serviceRequest');
+
+exports.GetRequestByUser = async (req, res) => {
+    try{
+        const {id} = req;
+        const result = await service.GetRequestByUser(id);
+        if(result.error){
+            return res.status(500).json({
+                err: true,
+                message: result.message
+            });
+        }
+        return res.status(200).json({
+            err: false,
+            data: result.result
+        });
+    }catch(error){
+        return res.status(500).json({
+            err: true,
+            message: error.message
+        });
+    }
+}
+
+exports.GetNonProcessedRequest = async (req, res) => {
+    try{
+        const result = await service.GetNonProcessedRequest();
+        if(result.error){
+            return res.status(500).json({
+                err: true,
+                message: result.message
+            });
+        }
+        return res.status(200).json({
+            err: false,
+            data: result.result
+        });
+    }catch(error){
+        return res.status(500).json({
+            err: true,
+            message: error.message
+        });
+    }
+}
+
+exports.GetRequestsToAdminView = async (req, res) => {
+    try{
+        const result = await service.GetRequestsToAdminView();
+        if(result.error){
+            return res.status(500).json({
+                err: true,
+                message: result.message
+            });
+        }
+        return res.status(200).json({
+            err: false,
+            data: result.result
+        });
+    }catch(error){
+        return res.status(500).json({
+            err: true,
+            message: error.message
+        });
+    }
+}
+
+exports.RespondRequest = async (req, res) => {
+    try{
+        const {id} = req;
+        const {idRequest, state, message, userEmail} = req.body;
+        const result = await service.RespondRequest(idRequest, state, id, message, userEmail);
+        if(result.error){
+            return res.status(500).json({
+                err: true,
+                message: result.message
+            });
+        }
+        return res.status(200).json({
+            err: false,
+            data: result.result
+        });
+    }catch(error){
+        return res.status(500).json({
+            err: true,
+            message: error.message
+        });
+    }
+}
+
+exports.GetProcessedRequests = async (req, res) => {
+    try{
+        const result = await service.GetProcessedRequest();
+        if(result.error){
+            return res.status(500).json({
+                err: true,
+                message: result.message
+            });
+        }
+        return res.status(200).json({
+            err: false,
+            data: result.result
+        });
+    }catch(error){
+        return res.status(500).json({
+            err: true,
+            message: error.message
+        });
+    }
+}
